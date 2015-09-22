7920a551285338b6951066721ff3ac41cdbefe42
ashot <arutune@gmail.com>
2015-09-22 14:51:58 -0700

    initial commit

--- model.js
+++
@@ -1,5 +0,0 @@
-Meteor.startup(function(){
-  // initialize models
-  Meteor.subscribe('posts');
-  Session.set('bz.search.offline', false);
-});
--- router.js
+++
@@ -1,6 +0,0 @@
-Router.map(function () {
-  this.route('search', {
-    path: '/search',
-    template: 'pageSearch'
-  });
-});
